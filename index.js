9a7d6f91a55bcb96e0a38e5e8757f0e33bcc4dee
Aliza <erjan984237@gmail.com>
2023-01-24 15:11:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+let myName = "Aliza"
+let myFamily = "Parents,litle brother,";
+let myCountry = "Kyrgyzstan";
+let myPhone = "Slmsung";
+let myFavoritebooks = "Cinderela";
+let myFavoriteanimals = "panda";
+let myLastname = "Shadykanova";
+let myNatsional = "kyrgyz";
+let mycolor = "yellow";
+let myHobby = "to read";
+
+let myNumber = 4;
+let mybirthday = 29;
+let myColor = 11;
+let myAdress = 39;
+let myheight = 170;
+
+let weitharbad = false;
+let moodgood = true;
+let myEnglishbad = true;
+
+let algebra = 5 + 5;
+let example1 = 6 - 6;
+let example2 = (4 + 5) - 2;
+let algebra2 = 90 * 7;
+let example3 = (60 - 30) * 5;
+let example4 = 16 / 4;
+let example5 = 5 * 7;
+
+let example6 = 20 %  5;
+let example7 = 45 % 7;
+
+let example8 = 8 > 4;
+let example9 = 0 > 1;
+let example10 = 9 = 9;
+let example11 = 4 < 2;
+let example12 = 10 = 10;
+let example13 = 520 > 400;
+   
+
+let a = 6, 
+b = 5, 
+c = 6;
+можно
+
+let a1 = 5;
+let b2 = 6;
+  
+
+
+
+
+ 
+
+
